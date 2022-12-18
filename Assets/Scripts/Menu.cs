646a1426543da28fbcac1d49eeed943ec714bc2b
MaloboKitano <malobo11@gmail.com>
2022-12-19 00:27:03 +0100

    Initial commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+	public GameObject setupCan;
+	public GameObject[] toggles = new GameObject[5];
+
+	void Start () { //start function
+	}
+	public void exit() { //exits the app
+		Application.Quit ();
+	}
+	public void setup(bool openClose) { //starts the setup canvas screen
+		setupCan.SetActive (openClose);			
+	}
+	public void play() { //finds the toggle that is on to decide how many ai players there will be
+		for (int i = 0; i < 5; i++) {
+			if (toggles [i].GetComponent<Toggle> ().isOn) {
+				Control.numbOfAI = i + 1;
+				break;
+			}				
+		}
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Main"); //then it loads the play screen
+	}
+	void Update() {
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Uno-y-Medio
+ First demo from the ultimate Uno y medio game
